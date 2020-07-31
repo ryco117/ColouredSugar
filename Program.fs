bca7983d0e3d488c83974440339f0525da47041c
Ryan Andersen <ryco117@gmail.com>
2020-07-30 18:30:23 -0700

    Updated Help Menu and improved UI auto scaling to fix aspect ratio

--- Program.fs
+++ Program.fs
@@ -65,8 +65,14 @@ type ColouredSugar() as world =
             let textureWidth, textureHeight =
                 match texture with
                 | {Width = width; Height = height; Data = _} ->
-                    2.f * min (float32 width / float32 world.Width) 0.95f,
-                    2.f * min (float32 height / float32 world.Height) 0.95f
+                    let aspect = (float32 width * float32 world.Height) / (float32 height * float32 world.Width)
+                    let f x = 2.f * min x 0.95f
+                    let w = f (float32 width / float32 world.Width)
+                    let h = f (float32 height / float32 world.Height)
+                    if (w / h - aspect) / (aspect + w / h) > 0.01f then
+                        aspect * h, h
+                    else
+                        w, w/aspect
             this.Position <- Vector3(-0.95f, 0.95f - textureHeight, 0.f)
             this.Scale <- Vector3(textureWidth, textureHeight, 1.f)
      }
